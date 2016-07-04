ad14cd9266e1766edec53d5a466cf120ef34d4b9
mertins <mertins@gmail.com>
2016-07-04 11:19:42 -0300

    Levando IA para projeto independente. Vai facilitar testes

--- ServerSocket/src/main/java/br/com/mertins/ufpel/fia/serversocket/ia/JogadorIA.java
+++ ServerSocket/src/main/java/br/com/mertins/ufpel/fia/serversocket/ia/JogadorIA.java
@@ -1,9 +1,22 @@
 package br.com.mertins.ufpel.fia.serversocket.ia;
 
+import br.com.mertins.ufpel.fia.gameengine.elements.Tabuleiro;
+import br.com.mertins.ufpel.fia.ia.util.Board;
+
 /**
  *
  * @author mertins
  */
 public class JogadorIA {
+    private final Board board;
+
+    public JogadorIA() {
+        this.board=new Board();
+    }
     
+    
+    public void setTabuleiro(Tabuleiro tabuleiro){
+        board.setTabuleiro(tabuleiro.getTabuleiro());
+        
+    }
 }
